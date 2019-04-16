fix ensemble to output / average softmaxes

sgld/ensemble.py
@@ -10,9 +10,11 @@ class Ensemble(torch.nn.Module):
     def __init__(self, models):
         super(Ensemble, self).__init__()
         self.models = models
+        self.softmax = torch.nn.Softmax()
 
     def forward(self, inputs):
+        """ returns the average of the softmaxed outputs for each model """
         # model(inputs) is BS x NUM_CLASSES
-        predictions = [model(inputs) for model in self.models]
+        predictions = [self.softmax(model(inputs)) for model in self.models]
         # final predictive distribution is just the mean of the softmax output
         return sum(predictions) / len(self.models)