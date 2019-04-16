c1385fbe4a4bba92fb3217c4054d9a3a1b4be85c
Noah Golmant <noah.golmant@gmail.com>
2019-04-15 18:49:45 -0700

    add entropy sanity check

--- sgld/utils.py
+++ sgld/utils.py
@@ -5,5 +5,17 @@ import torch
 
 def entropy(predictions):
     """ compute shannon entropy of the categorical prediction """
-    predictions = predictions / torch.sum(predictions, axis=1)
-    return -torch.sum(predictions * torch.log(predictions) / np.log(2), axis=1)
+    predictions = predictions / torch.sum(predictions, dim=1)
+    log_probs = torch.log(predictions)
+    # prevent nans with p(x) = 0
+    log_probs[log_probs == float("-Inf")] = 0.0
+    return -torch.sum(predictions * log_probs / np.log(2), dim=1)
+
+
+if __name__ == '__main__':
+    x = np.array([[.5, .5]])
+    x = torch.from_numpy(x)
+    assert entropy(x).item() == 1.
+    x = np.array([[1., 0]])
+    x = torch.from_numpy(x)
+    assert entropy(x).item() == 0.
